85e7f64a75cfcc4ed8aad342a670e92f65d9f7a8
rich-hart <rich.hart@rice.edu>
2015-11-19 19:51:09 -0600

    adding sql example

--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,21 @@
 from lxml import html
 import requests
 
-page = requests.get('http://houston.novusagenda.com/agendapublic/MeetingView.aspx?MeetingID=121&MinutesMeetingID=156&doctype=Minutes')
+import sqlite3 as lite
 
-print page.content
+url = 'http://houston.novusagenda.com/agendapublic/MeetingView.aspx?MeetingID=121&MinutesMeetingID=156&doctype=Minutes'
+
+page = requests.get(url)
+
+content = "this is web content"  #unicode(page.content)
 
 tree = html.fromstring(page.content)
 
 
+
+con = lite.connect('website.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Webdata(Url TEXT, Content TEXT)")
+    cur.execute("INSERT INTO Webdata VALUES( ?, ?)",(url,content))  
